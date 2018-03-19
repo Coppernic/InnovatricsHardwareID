a61f1a870f142407ba61c89e5bf7956760fca75a
Benoist Laforge <benoist.laforge@coppernic.fr>
2018-03-19 11:10:29 +0100

    [DEV] Implements #4, main view is created

--- app/src/main/java/fr/coppernic/innovatricshid/MainActivity.java
+++ app/src/main/java/fr/coppernic/innovatricshid/MainActivity.java
@@ -2,12 +2,26 @@ package fr.coppernic.innovatricshid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements MainView {
+
+    @BindView(R.id.tvHardwareId)
+    TextView tvHardwareId;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    public void showHardwareId(String hardwareId) {
+        tvHardwareId.setText(hardwareId);
     }
 }
